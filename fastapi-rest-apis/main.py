889de1ef01203c534c18a15f0b6a17099f8ce3f9
knight1001d <prathamjagga123@gmail.com>
2024-04-29 18:53:29 +0530

    add api for sequential flow

--- fastapi-rest-apis/main.py
+++ fastapi-rest-apis/main.py
@@ -1,10 +1,14 @@
-from typing import Union
+import subprocess
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return "Hello from Velocity - The soul of AutoMarket"
+@app.get("/sequential_flow")
+def run():
+    # TO DO: extract actions from request here
+    output = subprocess.check_output(["python", "../sequentialFlow.py", "../actions/readFile.py", "../actions/textSummarizer.py", "https://www.dwsamplefiles.com/?dl_id=176"])
+    return output
