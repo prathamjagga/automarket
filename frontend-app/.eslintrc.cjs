5470ca48999b281dcd31b5b4e96edf4be442493b
knight1001d <prathamjagga123@gmail.com>
2024-05-26 23:30:59 +0530

    minor refactor

--- frontend-app/.eslintrc.cjs
+++ frontend-app/.eslintrc.cjs
@@ -1,3 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
-const config = {};
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
+  rules: {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+  },
+};
 module.exports = config;
