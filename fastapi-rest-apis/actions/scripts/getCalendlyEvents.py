7f2697da19ebcedc0bebcae02dcbb2f5594d7e00
knight1001d <prathamjagga123@gmail.com>
2024-06-24 02:48:30 +0530

    start converting scripts to get_output

--- fastapi-rest-apis/actions/scripts/getCalendlyEvents.py
+++ fastapi-rest-apis/actions/scripts/getCalendlyEvents.py
@@ -2,12 +2,22 @@ import requests
 import sys
 import json
 
-try:
-    input_data = json.loads(sys.argv[1])
-    api_key = input_data["api_key"]
-    user_URI = input_data["user_URI"]
-    events = requests.get("https://api.calendly.com/scheduled_events?user="+user_URI, headers={"Authorization": "Bearer " + api_key})
-    print(json.dumps({ "output": {"type":"json", "content":events.json()}}))
+# try:
+#     input_data = json.loads(sys.argv[1])
+#     api_key = input_data["api_key"]
+#     user_URI = input_data["user_URI"]
+#     events = requests.get("https://api.calendly.com/scheduled_events?user="+user_URI, headers={"Authorization": "Bearer " + api_key})
+#     print(json.dumps({ "output": {"type":"json", "content":events.json()}}))
           
-except Exception as e:
-    print(json.dumps({"output": {"type": "error", "content": str(e)}}))
+# except Exception as e:
+#     print(json.dumps({"output": {"type": "error", "content": str(e)}}))
+
+
+def get_output(input_json):
+    try:
+        api_key = input_json["api_key"]
+        user_URI = input_json["user_URI"]
+        events = requests.get("https://api.calendly.com/scheduled_events?user="+user_URI, headers={"Authorization": "Bearer " + api_key})
+        return(json.dumps({ "output": {"type":"json", "content":events.json()}}))
+    except Exception as e:
+        return(json.dumps({"output": {"type": "error", "content": str(e)}}))
