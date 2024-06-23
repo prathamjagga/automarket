b34d7f0f4dc8dccff8e9d462437196dadf75d463
knight1001d <prathamjagga123@gmail.com>
2024-06-24 03:12:48 +0530

    minor fix

--- fastapi-rest-apis/actions/scripts/getCalendlyEvents.py
+++ fastapi-rest-apis/actions/scripts/getCalendlyEvents.py
@@ -2,16 +2,6 @@ import requests
 import sys
 import json
 
-# try:
-#     input_data = json.loads(sys.argv[1])
-#     api_key = input_data["api_key"]
-#     user_URI = input_data["user_URI"]
-#     events = requests.get("https://api.calendly.com/scheduled_events?user="+user_URI, headers={"Authorization": "Bearer " + api_key})
-#     print(json.dumps({ "output": {"type":"json", "content":events.json()}}))
-          
-# except Exception as e:
-#     print(json.dumps({"output": {"type": "error", "content": str(e)}}))
-
 
 def get_output(input_json):
     try:
