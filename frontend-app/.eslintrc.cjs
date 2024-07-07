0d0b1d5da139998cfc5eabc4ce4a16635ce1a4f8
knight1001d <prathamjagga123@gmail.com>
2024-07-07 20:23:36 +0530

    remove eslint

--- frontend-app/.eslintrc.cjs
+++ frontend-app/.eslintrc.cjs
@@ -5,18 +5,7 @@ const config = {
     project: true,
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  rules: {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": "off",
-  },
+  extends: [],
+  rules: {},
 };
 module.exports = config;
