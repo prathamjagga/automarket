6bbd68b74eab456200f3c7ab8ca15ad2fdedb7c5
knight1001d <prathamjagga123@gmail.com>
2024-07-08 07:31:57 +0530

    add readSpreadsheet

---
+++ fastapi-rest-apis/actions/scripts/readSpreadsheet.py
@@ -0,0 +1,9 @@
+def get_output(json_input):
+    import json
+    import pandas as pd
+    df = pd.read_excel(json_input["url"])
+    print(df)
+    column = df[json_input["column"]]
+    print(column.values.tolist())
+    return json.dumps({"output": {"type": "json", "content": {"array": ""}}})
+# get_output({"url":"https://file-examples.com/storage/feab15190e668af9da02618/2017/02/file_example_XLSX_50.xlsx", "column":"First Name"})
