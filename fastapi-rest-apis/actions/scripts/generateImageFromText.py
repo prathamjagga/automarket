6a0d0a3fee96659b37dfd5baa143275c9acb329d
knight1001d <prathamjagga123@gmail.com>
2024-07-08 08:22:55 +0530

    add generateImageFromText

--- fastapi-rest-apis/actions/scripts/generateImageFromText.py
+++ fastapi-rest-apis/actions/scripts/generateImageFromText.py
@@ -5,8 +5,9 @@ def get_output(input_json):
         from craiyon import Craiyon
         generator = Craiyon() # Instantiates the api wrapper
         result = generator.generate(input_json['prompt'], negative_prompt="spoon", model_type="art")
-        result.save_images()
-        return(json.dumps({"output": {"type": "text", "content": f"{result.description}"}}))
+        print(type(result[0]))
+        a = (json.dumps({"output": {"type": "text", "content": f"{result.images[0]}"}}))
+        return a
     except Exception as e:
         return(json.dumps({"output": {"type": "error", "content": str(e)}}))
         
