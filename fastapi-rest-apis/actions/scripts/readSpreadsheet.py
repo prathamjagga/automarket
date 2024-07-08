601eee3cdaf93a2792b1ca01fcc01c5036437f6e
knight1001d <prathamjagga123@gmail.com>
2024-07-08 07:39:51 +0530

    upd readSpreadsheet

--- fastapi-rest-apis/actions/scripts/readSpreadsheet.py
+++ fastapi-rest-apis/actions/scripts/readSpreadsheet.py
@@ -5,5 +5,5 @@ def get_output(json_input):
     print(df)
     column = df[json_input["column"]]
     print(column.values.tolist())
-    return json.dumps({"output": {"type": "json", "content": {"array": ""}}})
+    return json.dumps({"output": {"type": "json", "content": {"array": column.values.tolist()}}})
 # get_output({"url":"https://file-examples.com/storage/feab15190e668af9da02618/2017/02/file_example_XLSX_50.xlsx", "column":"First Name"})
