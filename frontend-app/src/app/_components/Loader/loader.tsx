9d8e10cb04bdeb86e883725202df2ff9f2235824
Yogesh Choudhary <yogesh986.be22@chitkara.edu.in>
2024-07-24 17:56:14 +0530

    adding loader

---
+++ frontend-app/src/app/_components/Loader/loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './loader.css'
+
+export default function loader() {
+  return (
+    <div className="container">
+	<div className="loader"></div>
+	<div className="loader"></div>
+	<div className="loader"></div>
+    </div>
+
+  )
+}
