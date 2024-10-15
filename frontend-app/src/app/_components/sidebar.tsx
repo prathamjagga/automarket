49339817d7403e15b1195bc77e78b2c125a87433
SHIWAM VISHWAKARMA <shiwam.vishwakarma@cloudeq.com>
2024-10-15 17:26:19 +0530

    UI changes

--- frontend-app/src/app/_components/sidebar.tsx
+++ frontend-app/src/app/_components/sidebar.tsx
@@ -2,24 +2,43 @@ import React from "react";
 
 import { Sidebar } from "flowbite-react";
 import { HiChartPie, HiViewBoards } from "react-icons/hi";
+import { MdOutlineDeveloperMode } from "react-icons/md";
+import { FaShoppingBag } from "react-icons/fa";
+import { PiPlugsConnectedFill } from "react-icons/pi";
 import Link from "next/link";
 
 function SidebarWrapper() {
   return (
-    <Sidebar style={{ height: "100vh" }} aria-label="Default sidebar example">
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Link href="create-run">
-            <Sidebar.Item icon={HiChartPie}>App Builder 👷‍♂️</Sidebar.Item>
-          </Link>
-          <Link href="market">
-            <Sidebar.Item icon={HiChartPie}>AutoMarket 🚀👜</Sidebar.Item>{" "}
-          </Link>
-          <Link href="connections">
-            <Sidebar.Item icon={HiChartPie}>Connections 🔌</Sidebar.Item>{" "}
-          </Link>
+    <div className="w-fit px-2 ">
+      <Sidebar style={{ height: "92vh", boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)"  }} aria-label="Default sidebar example" className="rounded-lg shadow-md">
+        <Sidebar.Items style={{ borderRadius: "10px"}}>
+          <Sidebar.ItemGroup>
+            <Link href="create-run">
+              <Sidebar.Item icon={MdOutlineDeveloperMode} className="hover:text-[#5072A7] hover:shadow-lg">
+                <div className="flex transform-gpu flex-row items-center justify-between transition-transform duration-300 ease-in-out hover:translate-x-3">
+                  <p>App Builder</p>
+                  {/* <p>👷‍♂️</p> */}
+                </div>
+              </Sidebar.Item>
+            </Link>
+            <Link href="market">
+              <Sidebar.Item icon={FaShoppingBag} className="hover:text-[#5072A7] hover:shadow-lg">
+                <div className="flex transform-gpu flex-row items-center justify-between transition-transform duration-300 ease-in-out hover:translate-x-3">
+                  <p>AutoMarket</p>
+                  {/* <p>👜</p> */}
+                </div>
+              </Sidebar.Item>{" "}
+            </Link>
+            <Link href="connections">
+              <Sidebar.Item icon={PiPlugsConnectedFill} className="hover:text-[#5072A7] hover:shadow-lg">
+                <div className="flex transform-gpu flex-row items-center justify-between transition-transform duration-300 ease-in-out hover:translate-x-3">
+                  <p>Connections</p>
+                  {/* <p>🔌</p> */}
+                </div>
+              </Sidebar.Item>{" "}
+            </Link>
 
-          {/* <Sidebar.Item
+            {/* <Sidebar.Item
             href="/run-apps"
             icon={HiViewBoards}
             // label="Pro"
@@ -27,11 +46,11 @@ function SidebarWrapper() {
           >
             Run Apps 🚀
           </Sidebar.Item> */}
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup
-          style={{ position: "absolute", bottom: "10px", left: "20px" }}
-        >
-          {/* <Sidebar.Item
+          </Sidebar.ItemGroup>
+          <Sidebar.ItemGroup
+            style={{ position: "absolute", bottom: "10px", left: "20px" }}
+          >
+            {/* <Sidebar.Item
             href="#"
             icon={HiViewBoards}
             // label="Pro"
@@ -42,9 +61,10 @@ function SidebarWrapper() {
           >
             Save flow as App
           </Sidebar.Item> */}
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+    </div>
   );
 }
 
